3945f1119950eed2e450cefdd3a52d1585d50bb0
Paco Preciado Mendoza <__pm@outlook.com>
2018-09-19 15:35:53 -0500

    :fire: Modify the model field of SalesModel : issue #5

--- MySales/MySales/Models/SalesModel.cs
+++ MySales/MySales/Models/SalesModel.cs
@@ -9,9 +9,7 @@ namespace MySales
             public int id_sale { get; set; }
             public string date { get; set; }
             public bool pay { get; set; }
-            [Indexed]
-            public int id_customer { get; set; }
-            [Indexed]
-            public int id_product { get; set; }
+            public string customer_name { get; set; }
+            public double sale_price { get; set; }
     }
 }
