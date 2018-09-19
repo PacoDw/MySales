f9d2d5e420c8fb6abe408f6f87fbab5c7a4935c7
Paco Preciado Mendoza <__pm@outlook.com>
2018-09-19 15:29:15 -0500

    :sparkles: Save into database in addSales : issue #3

--- MySales/MySales/Sales/AddSales.xaml.cs
+++ MySales/MySales/Sales/AddSales.xaml.cs
@@ -11,9 +11,13 @@ namespace MySales
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class AddSales : ContentPage
 	{
+        private string _dateTime;
+
 		public AddSales ()
 		{
 			InitializeComponent ();
+
+            this._dateTime = dateSale.Date.ToShortDateString();
 
             MessagingCenter.Subscribe<Customers, CustomersModel>(this, "context", (sender, arg) => {
                 customer_picture.Source = arg.customer_picture;
@@ -24,13 +28,24 @@ namespace MySales
             MessagingCenter.Subscribe<Products, ProductsModel>(this, "context", (sender, arg) => {
                 product_picture.Source = arg.product_picture;
                 product_name.Text = arg.product_name;
+                sale_price.Text = arg.sale_price.ToString();
                 id_product.Text = arg.id_product.ToString();
             });
         }
 
         private void saveSale_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Test", "Save a Sale", "ok");
+            var newSale = new SalesModel
+            {
+                date = _dateTime,
+                pay = paid.IsEnabled,
+                customer_name = customer_name.Text,
+                sale_price = double.Parse(sale_price.Text)
+            };
+            App.MySalesDB.InsertAsync(newSale);
+            DisplayAlert("Success!", "The sale was added!", "ok");
+
+            Navigation.PopAsync(true);
         }
 
         async private void addCustomer_Clicked(object sender, EventArgs e)
@@ -45,7 +60,7 @@ namespace MySales
 
         void OnDateSelected(object sender, DateChangedEventArgs args)
         {
-
+            this._dateTime = dateSale.Date.ToShortDateString();
         }
     }
 }
